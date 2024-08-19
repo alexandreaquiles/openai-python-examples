e86c10578d648369206a012b1c807bb0a336f542
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-08-19 17:22:41 -0300

    Aula 1

---
+++ main.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print(os.getenv('OPENAI_API_KEY'))
+cliente = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+resposta = cliente.chat.completions.create(
+  messages=[
+  {
+    "role": "system",
+    "content": "Listar apenas os nomes dos produtos, sem considerar descrição."
+  },
+  {
+    "role": "user",
+    "content": "Liste 3 produtos sustentáveis"
+  }
+  ],
+  model='gpt-3.5-turbo'
+)
+print(resposta.choices[0].message.content)

1976194097a11977b1412dbbc4a1362a380a6a9b
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-08-19 18:59:38 -0300

    Aula 4: comparando contagem de tokens com gpt-4o-mini e gpt-4o

--- contagem_tokens.py
+++ contagem_tokens.py
@@ -1,19 +1,19 @@
 import tiktoken
 
-modelo = "gpt-4"
+modelo_mini = "gpt-4o-mini"
+codificador = tiktoken.encoding_for_model(modelo_mini)
+lista_tokens = codificador.encode("Você é um categorizador de produtos.")
+
+print("Lista de Tokens: ", lista_tokens)
+print("Quantos tokens temos: ", len(lista_tokens))
+print(f"Custo para o modelo {modelo_mini} é de ${(len(lista_tokens)/1_000_000) * 0.15}")
+
+modelo = "gpt-4o"
 codificador = tiktoken.encoding_for_model(modelo)
 lista_tokens = codificador.encode("Você é um categorizador de produtos.")
 
 print("Lista de Tokens: ", lista_tokens)
 print("Quantos tokens temos: ", len(lista_tokens))
-print(f"Custo para o modelo {modelo} é de ${(len(lista_tokens)/1000) * 0.03}")
+print(f"Custo para o modelo {modelo} é de ${(len(lista_tokens)/1_000_000) * 5}")
 
-modelo = "gpt-3.5-turbo-1106"
-codificador = tiktoken.encoding_for_model(modelo)
-lista_tokens = codificador.encode("Você é um categorizador de produtos.")
-
-print("Lista de Tokens: ", lista_tokens)
-print("Quantos tokens temos: ", len(lista_tokens))
-print(f"Custo para o modelo {modelo} é de ${(len(lista_tokens)/1000) * 0.001}")
-
-print(f"O custo do GPT4 é de {0.03/0.001} maior que o do GPT 3.5-turbo")
+print(f"O custo do {modelo} é de {5/0.15} maior que o do {modelo_mini}")
